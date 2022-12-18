dc704f6169da743e133edbcd70979f94d499d454
tab.saruul <tab.saruul@gmail.com>
2022-12-18 16:45:07 +0800

    fix

--- prod/index.js
+++ prod/index.js
@@ -1,16 +1,42 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
@@ -21,10 +47,16 @@ const models_1 = require("./models");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4000;
-const main = () => __awaiter(void 0, void 0, void 0, function* () {
+const main = () =>
+  __awaiter(void 0, void 0, void 0, function* () {
     app.use((0, cookie_parser_1.default)());
     app.use(body_parser_1.default.urlencoded({ extended: false }));
-    app.use((0, cors_1.default)({ credentials: true, origin: "http://localhost:3000" }));
+    app.use(
+      (0, cors_1.default)({
+        credentials: true,
+        origin: "http://localhost:3000",
+      })
+    );
     // parse application/json
     app.use(body_parser_1.default.json());
     app.use("/auth", require("./routes/auth"));
@@ -32,9 +64,9 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
     app.use("/orlogo", require("./routes/orlogo"));
     app.use("/zarlaga", require("./routes/zarlaga"));
     app.get("/", (req, res) => {
-        res.send("we are on home");
+      res.send("we are on home");
     });
     yield (0, models_1.connectToDB)();
     app.listen(port);
-});
+  });
 main();
